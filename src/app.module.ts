bdd54e85d04e2addad6b2b95d9a07d3c2731c71c
t-kusakabe <ttmac0704@gmail.com>
2022-10-12 04:41:21 +0900

    Modify app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [TodoModule],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true
+    }),
+    TodoModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
