5e2196d66cd12f8efae066867e6263de29edef80
t-kusakabe <ttmac0704@gmail.com>
2022-10-12 04:24:34 +0900

    最低限作る

--- src/todo/todo.resolver.ts
+++ src/todo/todo.resolver.ts
@@ -1,4 +1,18 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, ID, Query, Resolver } from '@nestjs/graphql';
+import { Todo } from './models/todo.models';
+import { TodoService } from './todo.service';
 
 @Resolver()
-export class TodoResolver {}
+export class TodoResolver {
+  constructor(private todoService: TodoService) {}
+
+  @Query(() => [Todo], { nullable: 'items' })
+  findAll() {
+    return this.todoService.findAll();
+  }
+
+  @Query(() => Todo)
+  findOneById(@Args('id', { type: () => ID }) id: string) {
+    return this.todoService.findOneById(id);
+  }
+}
